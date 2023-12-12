2001b3786ab3224be1fb2de307ab232765cd4ce4
O-Pazukhanych <123318543+O-Pazukhanych@users.noreply.github.com>
2023-12-12 20:21:36 +0200

    hosting

--- functions/configDB.php
+++ functions/configDB.php
@@ -1,4 +1,4 @@
 <?php
 
-$dsn = 'mysql:host=localhost;dbname=to-do';
-$pdo = new PDO($dsn, 'root');
+$dsn = 'mysql:host=sql305.infinityfree.com;dbname=if0_35606818_todo';
+$pdo = new PDO($dsn, 'if0_35606818', 'HYlaOVf7RORR');
